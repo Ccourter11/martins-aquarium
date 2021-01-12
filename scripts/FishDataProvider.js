5b01a58b6ef4c1b8ab68eb65c231ce52bd49377c
Ccourter11 <courtercalvin@yahoo.com>
2021-01-12 15:08:52 -0600

    fixed , bugs in js

--- scripts/FishDataProvider.js
+++ scripts/FishDataProvider.js
@@ -3,31 +3,30 @@ const fishCollection = [
     name: "Lil Man",
     species: "Crappie",
     food: "crawfish",
-    length: "6-14 inches"
-
+    length: "6-14 inches",
   },
   {
     name: "Marlin",
     species: "Amphiprioninae",
     food: "seafood",
-    length: "2-6 inches"
-  }
+    length: "2-6 inches",
+  },
   {
     name: "Katie",
     species: "Large Mouth Bass",
     food: "bluegill,crawfish,snakes",
-    length: "8-16 inches" 
-  }
+    length: "8-16 inches",
+  },
   {
     name: "Kenny",
     species: "Salmon",
     food: "Mayflies",
-    length: "12-20 inches"
-  }
+    length: "12-20 inches",
+  },
   {
     name: "Marvin",
-    species: "Moorish Idol"
+    species: "Moorish Idol",
     food: "plankton",
-    length: "4-9 inches"
-  }
+    length: "4-9 inches",
+  },
 ];
