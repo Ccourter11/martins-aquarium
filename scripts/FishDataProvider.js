4c9794348d4062b6e61146302ef8d28744f0ef87
Ccourter11 <courtercalvin@yahoo.com>
2021-01-13 15:24:08 -0600

    added images to fish cards

--- scripts/FishDataProvider.js
+++ scripts/FishDataProvider.js
@@ -3,6 +3,7 @@ const fishCollection = [
     name: "Lil Man",
     species: "Crappie",
     diet: "crawfish",
+    location: "Southeast United States",
     length: "6-14 inches",
     image:
       "https://images.unsplash.com/photo-1506192135-2afa42cfdca9?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80",
@@ -10,26 +11,37 @@ const fishCollection = [
   {
     name: "Marlin",
     species: "Amphiprioninae",
+    location: "Great Barrier Reef, Australia",
     diet: "seafood",
     length: "2-6 inches",
+    image:
+      "https://www.aquariumofpacific.org/images/made_new/images-uploads-clownfish_600_q85.jpg",
   },
   {
     name: "Katie",
     species: "Large Mouth Bass",
+    location: "United States of America",
     diet: "bluegill,crawfish,snakes",
     length: "8-16 inches",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4ttvITVLfXG2A01Gwo1E-vBBj6qMwEPbavg&usqp=CAU",
   },
   {
     name: "Kenny",
     species: "Salmon",
+    location: "Alaska",
     diet: "Mayflies",
     length: "12-20 inches",
+    image:
+      "https://images.unsplash.com/photo-1574781330855-d0db8cc6a79c?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8c2FsbW9uJTIwZmlzaHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=60",
   },
   {
     name: "Marvin",
     species: "Moorish Idol",
     diet: "plankton",
+    location: "Southern Gulf of California south to Peru",
     length: "4-9 inches",
+    image: "moorishidol.jpg",
   },
 ];
 
