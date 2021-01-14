13213d05a6cedef13c22801f9276325b28ae19b1
Ccourter11 <courtercalvin@yahoo.com>
2021-01-13 20:12:20 -0600

    more edits

--- scripts/Fish.js
+++ scripts/Fish.js
@@ -1,6 +1,5 @@
 export const Fish = (fish) => {
-  return `
-  <div class="fishCard">
+  return `<div class="fishCard">
   <p class="fishCard__name">${fish.name}</p>
   <p class="fishCard__species">${fish.species}</p>
   <p class="fishCard__location">${fish.location}
@@ -8,10 +7,9 @@ export const Fish = (fish) => {
   <p class="fishCard__length">${fish.length}</p>
   <p class="fishCard__diet">${fish.diet}</p>
   <img
-    src=".${fish.image}"
+    src="${fish.image}"
     alt="fish image"
     class="fishCard__img"
   />
-  </div>
-    `;
+  </div>`;
 };
